dbb21e704c8167624cede3e821e2bfd172fce51b
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-01-05 09:23:47 +0100

    sedeplacer

--- public/js/main.js
+++ public/js/main.js
@@ -8,3 +8,13 @@ const Lieu = new Lieu('Molengeek')
 const Lieu = new Lieu('Snack')
 const Lieu = new Lieu('Maison')
 
+class personnages{
+    constructor(nom,prenom,argent){
+    this.nom = nom;
+    this.prenom = prenom;
+    this.argent = argent;
+    }
+    seDeplacer(Lieu)
+    this.
+
+}
