e10a51c3e98043184396afee13d4da0c80212527
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-01-05 09:12:19 +0100

    lieu

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+class Lieu {
+    constructor(nom,personnes){
+        this.nom = nom;
+        this.personnes = [];
+    }
+}
+const Lieu = new Lieu('Molengeek')
+const Lieu = new Lieu('Snack')
+const Lieu = new Lieu('Maison')
+
